0a3b76056265004bc319d25e42bf7769c1a3f928
llop10101 <joel.cano.puigvert@gmail.com>
2023-07-05 21:10:47 +0200

    updated: Fundació Esplai opens a new tab, scripts

--- src/components/NavigationBar/Dropdown/Dropdown2.js
+++ src/components/NavigationBar/Dropdown/Dropdown2.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { MenuItems } from '../MenuItems/MenuItems2';
 import './Dropdown.css';
-import { Link } from 'react-router-dom';
+import  { Link } from 'react-router-dom';
 
 function Dropdown() {
   const [click, setClick] = useState(false);
@@ -15,21 +15,39 @@ function Dropdown() {
         className={click ? 'dropdown-menu clicked' : 'dropdown-menu'}
       >
         {MenuItems.map((item, index) => {
-          return (
-            <li key={index}>
-              <Link
-                className={item.cName}
-                to={item.path}
-                onClick={() => setClick(false)}
-              >
-                {item.title}
-              </Link>
-            </li>
-          );
+          if (item.path.startsWith('http')) {
+            // Open in a new tab if it starts with 'http' or 'https'
+            return (
+              <li key={index}>
+                <a
+                  className={item.cName}
+                  href={item.path}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  onClick={() => setClick(false)}
+                >
+                  {item.title}
+                </a>
+              </li>
+            );
+          } else {
+            // Use the regular Link component for other paths
+            return (
+              <li key={index}>
+                <Link
+                  className={item.cName}
+                  to={item.path}
+                  onClick={() => setClick(false)}
+                >
+                  {item.title}
+                </Link>
+              </li>
+            );
+          }
         })}
       </ul>
     </>
   );
 }
 
-export default Dropdown;
+export default Dropdown
