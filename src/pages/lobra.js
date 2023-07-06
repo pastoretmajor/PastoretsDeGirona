73ee9619995452044597989733114b84c365a1bf
llop10101 <joel.cano.puigvert@gmail.com>
2023-07-07 00:24:20 +0200

    add: color specification

--- src/pages/lobra.js
+++ src/pages/lobra.js
@@ -18,11 +18,10 @@ const Lobra = () => {
     <div>
       <Navbar />
       <ImageComponent src={ObraBackground} alt="L'obra" text="L'obra" />
-
       <ColumnLayout boxes={boxes}  centered={false}/>
       <ColoredBox width="1440px" height="1120px" backgroundColor="#F1F1F1" src={Logo} alt="Logo" text="Després d'un any sense els Pastorets a la ciutat de Girona i amb la voluntat de tornar a representar-los durant quatre dies al Teatre Municipal, hem creat un nou text a on es barreja el món actual amb el passat. Els nous Pastorets continuaran essent de caire familiar i musical amb un format diferent, inclusiu i innovador." text2="L'obra combina humor, música, modernitat i tradició per explicar la història del naixement de Jesús i els diferents obstacles que van passant als personatges típics de la nostra cultura catalana"/>
-      <ColoredBox width="1440px" height="350px" backgroundColor="#D91E01" text="A través de la seva trama, Pastorets de Girona transmeten valors com l'amistat i la solidaritat."/>
-      <ColoredBox width="1440px" height="350px" backgroundColor="#F1F1F1"/>      
+      <ColoredBox textColor="#F1F1F1" width="1440px" height="350px" backgroundColor="#D91E01" text="A través de la seva trama, Pastorets de Girona transmeten valors com l'amistat i la solidaritat."/>
+      <ColoredBox width="1440px"  height="350px" backgroundColor="#F1F1F1" />      
       <Footer />
     </div>
   );
