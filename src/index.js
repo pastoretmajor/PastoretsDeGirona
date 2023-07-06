ef5594d26c58561d02f1baaf7182a9edb8762e22
llop10101 <joel.cano.puigvert@gmail.com>
2023-07-06 13:33:43 +0200

    add:Cache

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registered:', registration);
+      })
+      .catch((error) => {
+        console.log('Service Worker registration failed:', error);
+      });
+  });
+}
+
+
 ReactDOM.render(
   <React.StrictMode>
       <App />
