5684627d9de6287a5de8136039326d49f60e2340
llop10101 <joel.cano.puigvert@gmail.com>
2023-07-06 12:04:59 +0200

    add: hreflang default

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,16 +8,16 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-icons">
-        <a href="https://www.instagram.com" className='icon-instagram' aria-label="Link cap a l'instagram de l'associació Pastorets de Girona">
+        <a href="https://www.instagram.com" className='icon-instagram' aria-label="Link cap a l'instagram de l'associació Pastorets de Girona" hreflang="x-default">
           <FaInstagram className="icon" />
         </a>
-        <a href="https://www.facebook.com" className='icon-facebook' aria-label="Link cap a el facebook de l'associació Pastorets de Girona">
+        <a href="https://www.facebook.com" className='icon-facebook' aria-label="Link cap a el facebook de l'associació Pastorets de Girona" hreflang="x-default">
           <FaFacebook className="icon" />
         </a>
-        <a href="https://www.twitter.com" className='icon-twitter' aria-label="Link cap a el twitter de l'associació Pastorets de Girona">
+        <a href="https://www.twitter.com" className='icon-twitter' aria-label="Link cap a el twitter de l'associació Pastorets de Girona" hreflang="x-default">
           <FaTwitter className="icon" />
         </a>
-        <a href="https://www.tiktok.com" className='icon-tiktok' aria-label="Link cap a el tiktok de l'associació Pastorets de Girona">
+        <a href="https://www.tiktok.com" className='icon-tiktok' aria-label="Link cap a el tiktok de l'associació Pastorets de Girona" hreflang="x-default">
           <FaTiktok className="icon" />
         </a>
       </div>
--- src/components/TeamMemberCard/TeamMemberCard.js
+++ src/components/TeamMemberCard/TeamMemberCard.js
@@ -12,22 +12,22 @@ const TeamMemberCard = ({ name, role, bio, imageUrl, twitterLink, instagramLink,
         <p className="team-member-card__bio">{bio}</p>
         <div className="team-member-card__social">
           {twitterLink && (
-            <a href={twitterLink} target="_blank" rel="noopener noreferrer" className="icon" aria-label="Link cap a el twitter de la persona en questió">
+            <a href={twitterLink} target="_blank" rel="noopener noreferrer" className="icon" aria-label="Link cap a el twitter de la persona en questió" hreflang="x-default">
               <FaTwitter />
             </a>
           )}
           {instagramLink && (
-            <a href={instagramLink} target="_blank" rel="noopener noreferrer" className="icon" aria-label="Link cap a l'instagram de la persona en questió">
+            <a href={instagramLink} target="_blank" rel="noopener noreferrer" className="icon" aria-label="Link cap a l'instagram de la persona en questió" hreflang="x-default">
               <FaInstagram />
             </a>
           )}
           {facebookLink && (
-            <a href={facebookLink} target="_blank" rel="noopener noreferrer" className="icon" aria-label="Link cap a el facebook de la persona en questió">
+            <a href={facebookLink} target="_blank" rel="noopener noreferrer" className="icon" aria-label="Link cap a el facebook de la persona en questió" hreflang="x-default">
               <FaFacebook />
             </a>
           )}
           {tiktokLink && (
-            <a href={tiktokLink} target="_blank" rel="noopener noreferrer" className="icon" aria-label="Link cap a el tiktok de la persona en questió">
+            <a href={tiktokLink} target="_blank" rel="noopener noreferrer" className="icon" aria-label="Link cap a el tiktok de la persona en questió" hreflang="x-default">
               <FaTiktok />
             </a>
           )}
