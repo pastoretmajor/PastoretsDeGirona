14fc87cc2303e8dd65e8dd3b88dffc14727e7183
llop10101 <joel.cano.puigvert@gmail.com>
2023-07-07 00:35:47 +0200

    updated: boxes text and background color

--- src/pages/equip.js
+++ src/pages/equip.js
@@ -3,24 +3,23 @@ import Navbar from '../components/NavigationBar/Navbar'
 import Footer from '../components/Footer/Footer'
 import ImageComponent from '../components/ImageComponent/ImageComponent'
 import ColumnLayout from '../components/ColumnLayout/ColumnLayout';
-import {CenteredColoredBox, ColoredBox} from '../components/ColoredBox/ColoredBox';
+import {CenteredColoredBox} from '../components/ColoredBox/ColoredBox';
 import EquipHuma from '../images/backgroundImage.webP'
 import Logo from '../images/logoPastorets.png'
 const Equip = () => {
 
   const boxes = [
-    { width: '480px', height: '400px', backgroundColor: '#D90559', text: "Equip tècnic" },
-    { width: '480px', height: '400px', backgroundColor: '#79CD8D', text: "Actors i Actrius" },
-    { width: '480px', height: '400px', backgroundColor: '#8FCED0', text: "Ajudants de producció" },
+    { width: '480px', height: '400px', textColor:'#F1F1F1', backgroundColor: '#C41919', text: "Equip tècnic" },
+    { width: '480px', height: '400px', textColor:'#F1F1F1', backgroundColor: '#414141', text: "Actors i Actrius" },
+    { width: '480px', height: '400px', backgroundColor: '#D9D9D9', text: "Ajudants de producció" },
   ];
 
   return (
     <div>
       <Navbar />
       <ImageComponent src={EquipHuma} alt="Equip huma" text="Equip humà" />
-      <CenteredColoredBox width="1440px" height="530px" backgroundColor="#FFC55C" src={Logo} alt="Logo" text="L'Associació Pastorets de Girona, està formada per persones professionals dels diferents àmibts teatrals i persones amateurs amb una àmplia experiència en el món dels Pastorets de Girona"/>
+      <CenteredColoredBox width="1440px" height="530px" backgroundColor="#F1F1F1" src={Logo} alt="Logo" text="L'Associació Pastorets de Girona, està formada per persones professionals dels diferents àmibts teatrals i persones amateurs amb una àmplia experiència en el món dels Pastorets de Girona"/>
       <ColumnLayout boxes={boxes} centered={true}/>
-      <ColoredBox width="1440px" height="350px" backgroundColor="#fff"/> 
       <Footer />
     </div>
   )
