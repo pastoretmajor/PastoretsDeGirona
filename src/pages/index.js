856958934d2943f6fe3c84a59f46effa0b040edc
llop10101 <joel.cano.puigvert@gmail.com>
2023-07-07 13:01:11 +0200

    updated: imatgeCalendari

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Navbar from '../components/NavigationBar/Navbar'
 import ImageComponent from '../components/ImageComponent/ImageComponent'
 import AraSi from '../images/backgroundImageAraSi.webP'
-import Cartell from '../images/backgroundImage.webP'
+import ImatgeCalendari from '../images/calendariPastorets.webP'
 import Footer from '../components/Footer/Footer'
 import TeatreMunicipalInterior from '../images/teatreMunicipalInterior.webP'
 const Inici = () => {
@@ -9,7 +9,7 @@ const Inici = () => {
     <div>
       <Navbar />
       <ImageComponent src={AraSi} alt="ARA SÍ" text="ARA SÍ" />
-      <ImageComponent src={Cartell} alt="Cartell" text="" />
+      <ImageComponent src={ImatgeCalendari} alt="ImatgeCalendari" text="" />
       <ImageComponent src={TeatreMunicipalInterior} alt="Venta d'entrades" text="Venta d'entrades" />
       <Footer />
     </div>
