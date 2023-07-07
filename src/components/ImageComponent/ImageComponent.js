60a0e04c7e78c82d5c0710e5bd00a8aed5dd295c
llop10101 <joel.cano.puigvert@gmail.com>
2023-07-07 14:15:48 +0200

    add: blur loading and lazyLoading

--- src/components/ImageComponent/ImageComponent.js
+++ src/components/ImageComponent/ImageComponent.js
@@ -1,18 +1,53 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import './ImageComponent.css';
 
-const ImageComponent = ({ src, text }) => {
+const ImageComponent = ({ src, blurSrc, text }) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const imageRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting || entry.intersectionRatio > 0) {
+          setIsLoaded(true);
+          observer.unobserve(entry.target);
+        }
+      });
+    });
+
+    const currentImageRef = imageRef.current;
+
+    if (currentImageRef) {
+      observer.observe(currentImageRef);
+    }
+
+    return () => {
+      if (currentImageRef) {
+        observer.unobserve(currentImageRef);
+      }
+    };
+  }, []);
+
+  const handleLoad = () => {
+    setIsLoaded(true);
+  };
+
   const divStyle = {
-    backgroundImage: `url(${src})`,
+    backgroundImage: `url(${isLoaded ? src : blurSrc})`,
   };
 
   return (
     <div className="content-container">
-      <div className="image-component" style={divStyle}></div>
+      <div
+        className="image-component"
+        style={divStyle}
+        ref={imageRef}
+        onLoad={handleLoad}
+      ></div>
       {text && (
         <div className="text-overlay">
-        <h1>{text}</h1>
-      </div>
+          <h1>{text}</h1>
+        </div>
       )}
     </div>
   );
