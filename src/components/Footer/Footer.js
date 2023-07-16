400b70a5d11ccea4b57b6352e0ec856c05a10087
llop10101 <joel.cano.puigvert@gmail.com>
2023-07-16 22:49:40 +0200

    updated: footerlinks

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import './Footer.css';
 import { FaInstagram, FaFacebook, FaTwitter, FaTiktok } from 'react-icons/fa';
-import { IoMail, IoCall } from 'react-icons/io5';
 
 const Footer = () => {
     const email = 'pastoret.major@pastoretsdegirona.cat'
   return (
     <footer className="footer">
       <div className="footer-icons">
-        <a href="https://www.instagram.com" className='icon-instagram' aria-label="Link cap a l'instagram de l'associació Pastorets de Girona" hrefLang="x-default">
+        <a href="https://www.instagram.com/elspastoretsdegirona/" target="_blank" rel="noopener noreferrer" className='icon-instagram' aria-label="Link cap a l'instagram de l'associació Pastorets de Girona" hrefLang="x-default">
           <FaInstagram className="icon" />
         </a>
-        <a href="https://www.facebook.com" className='icon-facebook' aria-label="Link cap a el facebook de l'associació Pastorets de Girona" hrefLang="x-default">
+        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className='icon-facebook' aria-label="Link cap a el facebook de l'associació Pastorets de Girona" hrefLang="x-default">
           <FaFacebook className="icon" />
         </a>
-        <a href="https://www.twitter.com" className='icon-twitter' aria-label="Link cap a el twitter de l'associació Pastorets de Girona" hrefLang="x-default">
+        <a href="https://twitter.com/elspastoretsgi?t=o5SkbfuB65jR5MuChWxT5g&s=08" target="_blank" rel="noopener noreferrer" className='icon-twitter' aria-label="Link cap a el twitter de l'associació Pastorets de Girona" hrefLang="x-default">
           <FaTwitter className="icon" />
         </a>
-        <a href="https://www.tiktok.com" className='icon-tiktok' aria-label="Link cap a el tiktok de l'associació Pastorets de Girona" hrefLang="x-default">
+        <a href="https://www.tiktok.com" target="_blank" rel="noopener noreferrer" className='icon-tiktok' aria-label="Link cap a el tiktok de l'associació Pastorets de Girona" hrefLang="x-default">
           <FaTiktok className="icon" />
         </a>
       </div>
