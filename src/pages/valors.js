ec84795f634f8d102ab6fe7bd389209a538c7dc7
llop10101 <joel.cano.puigvert@gmail.com>
2023-07-16 22:31:36 +0200

    updated: valors content

--- src/pages/valors.js
+++ src/pages/valors.js
@@ -10,12 +10,12 @@ import BlurValorsImatge from '../images/alaAngelBlancBlur.webP'
 
 const Valors = () => {
   const circles = [
-    { defaultText: 'Valor 1', hoverText: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lectus nisl, blandit ornare fringilla accumsan, elementum id sapien. Integer vitae egestas nulla, at auctor tellus. Vestibulum finibus rhoncus molestie. Curabitur non lorem et ante pellentesque elementum. Etiam sed tortor ullamcorper augue posuere pharetra varius at libero. Nam euismod nisl velit. Curabitur nec ultrices est. Proin nec orci ac urna vulputate convallis. Integer ex leo, efficitur eget euismod vitae, ultrices non erat. Nunc blandit, turpis sit amet commodo consectetur, nunc metus luctus ante, sed congue nunc mauris ac felis. Morbi hendrerit lectus placerat felis vestibulum, eu dapibus lectus cursus.' },
-    { defaultText: 'Valor 2', hoverText: 'Lorem Ipsum Dolor Sit Amet' },
-    { defaultText: 'Valor 3', hoverText: 'Lorem Ipsum Dolor Sit Amet' },
-    { defaultText: 'Valor 4', hoverText: 'Lorem Ipsum Dolor Sit Amet' },
-    { defaultText: 'Valor 5', hoverText: 'Lorem Ipsum Dolor Sit Amet' },
-    { defaultText: 'Valor 6', hoverText: 'Lorem Ipsum Dolor Sit Amet' },
+    { defaultText: 'Emprenedoria i Innovació', hoverText: 'Plantegem els reptes com a oportunitats per crear una innovadora representació teatral sobre Nadal. Mirem cap al futur i ens enfoquem en aspectes de l’actualitat.' },
+    { defaultText: 'Girona', hoverText: 'Estimem Girona i la seva gent. Per això treballem per promoure la representació teatral a la ciutat i interpretat a través de la seva gent, sempre obert a tothom qui hi vulgui participar.' },
+    { defaultText: 'Paritat i perspectiva de gènere', hoverText: 'Tenim el compromís d’assolir espais d’activitat segurs, unes relacions basades en la llibertat i el respecte entre les persones i conseqüentment lliures de situacions d’assetjament sexual i per raó de sexe.' },
+    { defaultText: 'Sostenibilitat', hoverText: 'Apostem per la sostenibilitat a través de diferents aspectes dins i fora l’escenari, reduint al màxim l’impacte ambiental del material utilitzat per l’escenari, vestuari i il·luminació.' },
+    { defaultText: 'Tradicions catalanes', hoverText: 'Vetllem per la perduració de la cultura catalana a la nostra ciutat. Els pastorets, com  representació nadalenca, tenen els seus orígens des de l’època medieval fins avui en dia.' },
+    { defaultText: 'Transparència', hoverText: 'Creiem en la importància del principi de transparència i de les bones pràctiques com a forma de mostrar la gestió de l’Associació Pastorets de Girona, així com de l’assoliment de la finalitat per la qual ha estat constituïda. També estem compromesos amb tots els socis, sòcies i membres de l’associació, i per tant oferim una informació clara i transparent sobre la gestió d’aquesta associació.' },
   ]
 
   return (
