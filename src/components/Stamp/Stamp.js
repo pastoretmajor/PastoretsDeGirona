17070f2cd24a0985faa2fe5e6fd1276559953780
llop10101 <joel.cano.puigvert@gmail.com>
2023-07-26 12:47:47 +0200

    add: ara sí stamp

---
+++ src/components/Stamp/Stamp.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import StampAraSi from "../../images/logos/ara-si-segell.png";
+import "./Stamp.css";
+
+const Stamp = () => {
+  const [showImage, setShowImage] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // Obtenir la posició actual de la finestra en la pantalla
+      const scrollY = window.scrollY;
+      
+      // Si la posició és a la part superior de la pàgina, mostrem la imatge
+      if (scrollY <= 250) {
+        setShowImage(true);
+      } else {
+        setShowImage(false);
+      }
+    };
+
+    // Afegim un esdeveniment d'escolta d'escorçament a la finestra
+    window.addEventListener("scroll", handleScroll);
+
+    // Comprovem la posició de la finestra al carregar la pàgina
+    handleScroll();
+
+    // Eliminem l'esdeveniment d'escolta quan el component es desmunti
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <>
+      {showImage && (
+        <img
+          src={StampAraSi}
+          alt="Imatge Fixa"
+          className="fixed-image"
+        />
+      )}
+    </>
+  );
+};
+
+export default Stamp;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Navbar from '../components/NavigationBar/Navbar'
 import Burger from '../components/Burger/Burger'
 
 import ImageComponent from '../components/ImageComponent/ImageComponent'
+import Stamp from '../components/Stamp/Stamp'
 import Footer from '../components/Footer/Footer'
 import PatrocinadorsPrincipals from '../components/PatrocinadorsPrincipals/PatrocinadorsPrincipals'
 
@@ -17,6 +18,7 @@ const Inici = () => {
       <Navbar />
       <Burger />      
       <ImageComponent src={AraSi} alt="Pastorets de Girona ara sí" text="Pastorets de Girona"/>
+      <Stamp />
       <ImageComponent src={ImatgeCalendari} blurSrc={BlurImatgeCalendari} alt="ImatgeCalendari" text="" />
       <ImageComponent src={TeatreMunicipalInterior} blurSrc={BlurTeatreMunicipalInterior} alt="Venta d'entrades" text="Venta d'entrades" />
       <PatrocinadorsPrincipals />
