15e0fe04e8fea57b521bfd51b5401e650ef519e3
llop10101 <joel.cano.puigvert@gmail.com>
2023-08-04 19:57:56 +0200

    add: component donacio

---
+++ src/components/Donacio/Donacio.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Donacio.css'; // Importa el archivo CSS para aplicar estilos
+
+const Donacio = () => {
+  return (
+    <div>
+        <div className='container'>
+            <div className='text'>
+            Per fer-te <b><i>"amic o amiga"</i></b> dels Pastorets de Girona, pots fer una donació de l'import que tu triïs al compte de CaixaBank indicat a continuació:
+            </div>
+            <h2>ES82 2100 0270 45 0200292373</h2>
+            <h4>Beneficiari: Fundació Esplai Girona</h4>
+        </div> 
+    </div>
+    
+  );
+};
+
+export default Donacio;
