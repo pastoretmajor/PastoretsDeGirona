40a4a4699971208e511be1fb12e975719326370f
llop10101 <joel.cano.puigvert@gmail.com>
2023-08-12 13:23:57 +0200

    updated: images load faster

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,8 +1,8 @@
 // Calendari.js
 import React from 'react';
 import './Calendar.css';
-import imatgeFonsCalendari from '../../images/banners/alaAngelCalendari.png';
-import calendarImage from '../../images/banners/desembre.png'
+import imatgeFonsCalendari from '../../images/banners/alaAngelCalendari.webP';
+import calendarImage from '../../images/banners/desembre.webP'
 const Calendari = () => {
   return (
     <div className="calendari-container">
