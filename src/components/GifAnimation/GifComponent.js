2893eb481ca8e8cad4e413e0e151ee23225fc1fe
llop10101 <joel.cano.puigvert@gmail.com>
2023-08-24 01:57:50 +0200

    add: manela gif made by: Adrià Cano Puigvert

--- src/components/GifAnimation/GifComponent.js
+++ src/components/GifAnimation/GifComponent.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import './GifComponent.css'
+import React, { useState } from 'react';
+import './GifComponent.css';
+
 const GifComponent = ({ gif }) => {
+  const [resetCount, setResetCount] = useState(0);
+
+  const handleGifReset = () => {
+    setResetCount(prevCount => prevCount + 1);
+  };
+
   return (
-    <div className="gif-component">
+    <div className="gif-container">
       <img
+        key={resetCount} // Esto hace que la imagen se reinicie cada vez que resetCount cambie
         src={gif}
         alt="Animated GIF"
         className="gif"
+        onAnimationIteration={handleGifReset} // Llamada cuando se reinicia la animación
       />
     </div>
   );
--- src/pages/manela.js
+++ src/pages/manela.js
@@ -6,7 +6,7 @@ import Footer from '../components/Footer/Footer'
 import GifComponent from '../components/GifAnimation/GifComponent'
 import PatrocinadorsPrincipals from '../components/PatrocinadorsPrincipals/PatrocinadorsPrincipals'
 
-import gifUrl from '../images/logos/pas1.png'
+import gifUrl from '../images/logos/manela_adria_cano_puigvert.gif'
 import LaManela from '../images/banners/lamanela-pastorets-de-girona.webP'
 import LaManelaBlur from '../images/blured/lamanela-pastorets-de-girona.webP'
 
