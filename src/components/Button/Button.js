53567a486861868b335e1996605f814400ce956b
llop10101 <joel.cano.puigvert@gmail.com>
2023-10-18 10:10:54 +0200

    updated: button style

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Button.css';
-import { Link } from 'react-router-dom';
 
 export function Button() {
   return (
-    <Link to='../entrades'>
-      <button className='btn'>ENTRADES</button>
-    </Link>
+    <a class="fancy" href='../#/entrades'>
+      <span class="top-key"></span>
+      <span class="text">ENTRADES</span>
+      <span class="bottom-key-1"></span>
+      <span class="bottom-key-2"></span>
+    </a>
   );
 }
