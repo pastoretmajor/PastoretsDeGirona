490ec0969c2963f05ba22a7d958c645069ca0432
llop10101 <joel.cano.puigvert@gmail.com>
2023-10-26 21:14:04 +0200

    updated: service-worker file

--- service-worker.js
+++ service-worker.js
@@ -2,20 +2,9 @@ const CACHE_NAME = 'my-cache';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/logoPastorets.webP',
+  '/favicon.ico',
   '/manifest.json',
   '/robots.txt',
-  '/styles.css',
-  '/static/js/bundle.js', // Adjust the path to your main JavaScript bundle
-  '/static/css/main.cd43fc8e.css', // Adjust the path to your main CSS bundle
-  '/static/media/angel.webP',
-  '/static/media/castingIcon.webP',
-  '/static/media/devil.webP',
-  '/static/media/logoHoritzontal.webP',
-  '/static/media/logoPastorets.webP',
-  '/static/media/pastor.webP',
-  '/static/media/questionMarkIcon.webP',
-  '/static/media/Teatre.webP',
 ];
 
 this.addEventListener('install', (event) => {
