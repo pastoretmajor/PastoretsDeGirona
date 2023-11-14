1bfc89a401d77487eed2fcc2eba10de5fe4cb847
llop10101 <joel.cano.puigvert@gmail.com>
2023-11-14 21:19:57 +0100

    updated: triying to index the video

--- src/components/Poster/Poster.js
+++ src/components/Poster/Poster.js
@@ -3,10 +3,10 @@ import videoPoster from '../../videos/videoPosterPastorets.mp4'
 import imatgePoster from '../../images/logos/Poster.jpg'
 
 const Poster = () => {
-    return (
-      <div>
-        <h2>Ja tenim el pòster d'aquest any 2023!</h2>
-        <div className="poster">
+return (
+    <section className="poster-section">
+      <h2>Ja tenim el pòster d'aquest any 2023!</h2>
+      <div className="poster">
         <img src={imatgePoster} alt="Pòster" />
         <video controls>
           <source src={videoPoster} type="video/mp4" />
@@ -14,8 +14,8 @@ const Poster = () => {
         </video>
       </div>
       <p className="descripcio">L'11 de novembre, vam presentar el cartell d'aquest any a la Plaça dels Apòstols.<br />L'autor del cartell Pau Morales, va estar present juntament amb el regidor de cultura Quim Ayats.<br /> També hi van participar les ballarines de Girona En Dansa, oferint una trepidant demostració del que faran enguany en col·laboració amb els Pastorets de Girona.</p>
-      </div>
-    );
-  };
+    </section>
+  );
+}
 
   export default Poster
