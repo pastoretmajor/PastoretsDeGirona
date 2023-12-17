17ac37a45997173d2b8213714654b3420c9314ec
llop10101 <joel.cano.puigvert@gmail.com>
2023-12-17 19:03:50 +0100

    add: text

--- src/components/CartaDirector/CartaDirector.js
+++ src/components/CartaDirector/CartaDirector.js
@@ -10,17 +10,26 @@ const Carta = () => {
         </div>
         <div className="director-content">
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.
-            Sed nisi.
+            Els nous Pastorets de Girona estan fets amb molta il·lusió.
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.
-            Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper
-            porta. Mauris massa. Vestibulum lacinia arcu eget nulla.
+            Tot comença de nou: el text, l'escenografia, els vesits, la música...
+          </p>
+          <p>
+            Des del voluntariat hem dedicat temps i esforços per preparar un espectacle nadalenc agradable a petits i a grans.
+          </p>
+          <p>
+            Moltes persones hem anat teixint grans complicitats fins arribar a les funcions que presentem aquest any amb el desig que passeu una bona estona.
+          </p>
+          <p>
+            Amb aquestes línies volem agrair la vostra confiança, sense la qual no seria possible aquest projecte.
+          </p>
+          <p>
+            Esperem retrobar-nos ben aviat
           </p>
         </div>
         <div className="director-footer">
-          <p className="director-signature">Atentament,<br />El Director</p>
+          <p className="director-signature">Atentament,<br />els directors</p>
         </div>
       </div>
     </div>
